32458a24894c150fa1d47e982da703e583285017
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-08-19 01:18:08 +0530

    feat: add context in navbar

--- nst-fe/src/components/navbar/NavLinks.tsx
+++ nst-fe/src/components/navbar/NavLinks.tsx
@@ -1,9 +1,17 @@
 "use client";
+
+import { useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useStore } from "@/app/store";
+import Button from "../Button";
+import { UserRound } from "lucide-react";
 
 export default function NavLinks() {
 	const pathName = usePathname();
+	const { isLoggedIn, name } = useStore();
+
+	useEffect(() => {}, [isLoggedIn]);
 
 	const navLinks = [
 		{
@@ -21,10 +29,6 @@ export default function NavLinks() {
 		{
 			title: "Themes",
 			path: "/themes",
-		},
-		{
-			title: "My Account",
-			path: "/account",
 		},
 	];
 
@@ -47,6 +51,35 @@ export default function NavLinks() {
 					</span>
 				</Link>
 			))}
+			{isLoggedIn ? (
+				<Link
+					href={"/account"}
+					className="group transition-all duration-300 ease-in-out"
+				>
+					<span
+						className={`relative after:bg-skin after:absolute after:h-1 after:w-0 after:-bottom-4 after:left-0 hover:after:w-full after:transition-all after:duration-500 after:rounded ${
+							pathName == "/account"
+								? "text-skin after:w-full"
+								: ""
+						}`}
+					>
+						<span className="flex justify-between items-center gap-5 text-sm">
+							<UserRound /> Hi, {name.split(" ")[0]}
+						</span>
+					</span>
+				</Link>
+			) : (
+				<Link
+					href={"/login"}
+					className="group transition-all duration-300 ease-in-out"
+				>
+					<Button
+						text="Sign In"
+						type="button"
+						className="text-darker"
+					/>
+				</Link>
+			)}
 		</>
 	);
 }
--- nst-fe/src/components/navbar/Navbar.tsx
+++ nst-fe/src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
 	return (
 		<nav className="w-full py-5 px-10 flex justify-between bg-dark text-xl text-light">
 			<header className="grow">Logo</header>
-			<section className="flex grow justify-around">
+			<section className="flex grow justify-around items-center">
 				<NavLinks />
 			</section>
 		</nav>

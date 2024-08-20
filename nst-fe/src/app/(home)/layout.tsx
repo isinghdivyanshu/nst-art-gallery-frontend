984253840ce709f81db7133946e4337eaa760786
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-08-20 23:11:56 +0530

    feat: custom scrollbar

--- nst-fe/src/app/(home)/layout.tsx
+++ nst-fe/src/app/(home)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<main className="h-screen overflow-x-hidden">
+		<main className="min-h-screen">
 			<Navbar />
 			{children}
 		</main>

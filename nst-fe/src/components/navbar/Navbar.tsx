d1bac6d0cdf7f31775ee670cf3e500bd02661e27
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-08-21 15:57:07 +0530

    fix: navbar

--- nst-fe/src/components/navbar/Navbar.tsx
+++ nst-fe/src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import NavLinks from "./NavLinks";
 
 export default function Navbar() {
 	return (
-		<nav className="w-full py-5 px-10 flex justify-between bg-dark text-xl text-light">
+		<nav className="w-full py-5 px-10 flex justify-between bg-dark text-xl text-light h-[4.25rem]">
 			<header className="grow">Logo</header>
 			<section className="flex grow justify-around items-center">
 				<NavLinks />

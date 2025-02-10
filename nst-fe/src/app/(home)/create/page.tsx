891ec4f647ab7df6ccb05515045fe2d57f66ef41
Dev Keshwani <dev.keshwani345@gmail.com>
2025-02-10 14:31:37 +0530

    feat: art creation added

--- nst-fe/src/app/(home)/create/page.tsx
+++ nst-fe/src/app/(home)/create/page.tsx
@@ -4,8 +4,14 @@ import { useRef, useState } from "react";
 import Button from "@/components/Button";
 import Image from "next/image";
 import CallModal from "@/components/modal/CallModal";
+import { useRouter } from "next/navigation";
+import { createArt } from "@/services/service";
+import { toast } from "sonner";
+import { useArtStore } from "@/store/ArtStore";
 
 export default function CreateArt() {
+	const router = useRouter();
+	const [isPending, setIsPending] = useState<boolean>(false);
 	const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 	const contentFileInput = useRef<HTMLInputElement>(null);
 	const styleFileInput = useRef<HTMLInputElement>(null);
@@ -63,7 +69,7 @@ export default function CreateArt() {
 	};
 
 	return (
-		<main className="min-h-[calc(100svh-4.25rem)] w-screen bg-dark">
+		<form className={`min-h-[calc(100svh-4.25rem)] w-screen bg-dark ${isPending ? "cursor-wait" : "cursor-auto"}`}>
 			<section className="flex flex-col lg:flex-row gap-20 items-center">
 				<article className="flex flex-col justify-center lg:items-end  w-1/2 p-16 xl:p-28">
 					<div className="flex flex-col justify-normal items-center gap-5">
@@ -198,6 +204,8 @@ export default function CreateArt() {
 					type="button"
 					text="Stylize"
 					className="font-medium text-light bg-soil px-14 mb-20"
+					disabled={isPending}
+					onClick={handleStylize}
 				/>
 			</div>
 			<CallModal
@@ -205,6 +213,72 @@ export default function CreateArt() {
 				isOpen={modalIsOpen}
 				onReqClose={toggleModal}
 			/>
-		</main>
+		</form>
 	);
+
+	async function handleStylize(event: any) {
+		event.preventDefault();
+		setIsPending(true);
+
+		try {
+			const contentFile = contentFileInput.current?.files?.[0];
+			const styleFile = styleFileInput.current?.files?.[0];
+
+			if (!contentFile || !styleFile) {
+				toast.error("Please select both images");
+				return;
+			}
+
+			const content_image = await new Promise<string>((resolve) => {
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					const base64String = reader.result as string;
+					resolve(base64String.split(',')[1]);
+				};
+				reader.readAsDataURL(contentFile);
+			});
+
+			const style_image = await new Promise<string>((resolve) => {
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					const base64String = reader.result as string;
+					resolve(base64String.split(',')[1]);
+				};
+				reader.readAsDataURL(styleFile);
+			});
+
+			const token = localStorage.getItem("token");
+			if (!token) {
+				toast.error("Please login to stylize art");
+				return;
+			}
+
+			const stylize = await createArt({ token, content_image, style_image });
+
+			if (stylize?.status === "success") {
+				toast.success(stylize.message);
+
+				const base64Response = stylize.image;
+				const response = await fetch(base64Response);
+				const blob = await response.blob();
+
+				// Create a File object from the Blob
+				const stylizedFile = new File([blob], 'stylized-art.jpg', {
+					type: 'image/jpg',
+					lastModified: Date.now()
+				});
+
+				// Store the File object in Zustand
+				useArtStore.getState().setStylizedImage(stylizedFile);
+				router.push("/preview");
+			} else if (stylize?.status === "error") {
+				toast.error(stylize.message);
+			}
+		} catch (error) {
+			toast.error("An error occurred. Please try again later.");
+			console.error(error);
+		} finally {
+			setIsPending(false);
+		}
+	}
 }

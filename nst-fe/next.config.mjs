76f2ab8783756162f8a3e72105c7a60794da0793
Dev Keshwani <dev.keshwani345@gmail.com>
2025-02-11 05:38:15 +0530

    fix: nextjs server configs

--- nst-fe/next.config.mjs
+++ nst-fe/next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '8000',
-            pathname: '/**',
-          },
-        ],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**',
       },
+    ],
+  },
 };
 
 export default nextConfig;
